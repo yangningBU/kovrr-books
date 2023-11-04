24f554fbfe26eb749070e04096aa4a91b07638e3
Yonatan Laurence <laurence0521@gmail.com>
2023-11-04 20:02:38 +0200

    Refactor MaxResult

--- src/catalog/Catalog.jsx
+++ src/catalog/Catalog.jsx
@@ -16,7 +16,6 @@ const Catalog = () => {
 
   const dispatch = useDispatch()
   const [maxResultCount, setMaxResultCount] = useState(DEFAULT_MAX_RESULT_COUNT)
-  /* FIXME: extract to method */
   const [pageIndex, setPageIndex] = useState(0)
   const offset = pageIndex * maxResultCount
   const showPrevious = pageIndex > 0
@@ -32,29 +31,29 @@ const Catalog = () => {
       .catch(error => dispatch({type: LOAD_ERROR}))
   }, [dispatch, maxResultCount, offset])
 
+  const MaxResult = ({count = 10}) => (
+    <button
+      onClick={() => setMaxResultCount(count)}
+      className={maxResultCount === count ? 'selected' : undefined}
+    >
+      {count}
+    </button>
+  )
+
   return (
     <div id="catalogWrapper">
       <h1>Catalog</h1>
       <div className="controls">
         <span>Result Size: </span>
-        <button
-          onClick={() => setMaxResultCount(10)}
-          className={maxResultCount === 10 ? 'selected' : undefined}
-        >10</button>
-        <button
-          onClick={() => setMaxResultCount(25)}
-          className={maxResultCount === 25 ? 'selected' : undefined}
-        >25</button>
-        <button
-          onClick={() => setMaxResultCount(40)}
-          className={maxResultCount === 40 ? 'selected' : undefined}
-        >40</button>
+        <MaxResult count={10} />
+        <MaxResult count={25} />
+        <MaxResult count={40} />
       </div>
       <div className="controls">
         <button onClick={() => setPageIndex(pageIndex - 1)} disabled={!showPrevious}>Prev</button>
         <span>Page {pageIndex + 1}</span>
         <button onClick={() => setPageIndex(pageIndex + 1)}>Next</button>
-      </div>
+      </div> 
       <div id="scroll">
         <div className="catalog">
           {books.map(book => <CatalogBook book={book} key={book.id}/>)}
