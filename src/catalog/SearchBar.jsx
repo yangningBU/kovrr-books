b06aa0c500ae0b1cd293dc92563c866fb74e4dfe
Yonatan Laurence <laurence0521@gmail.com>
2023-11-05 15:57:57 +0200

    Remove console.log

--- src/catalog/SearchBar.jsx
+++ src/catalog/SearchBar.jsx
@@ -1,7 +1,6 @@
 const SearchBar = ({search, handleChange}) => {
   const onChange = (e) => {
     const newValue = e.target.value
-    console.log("Searching: ", newValue)
     handleChange(newValue)
   }
   return (
