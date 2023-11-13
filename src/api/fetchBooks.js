48ffeb99fec5b1924024e7aff5cf0ac6c0daa614
Yonatan Laurence <laurence0521@gmail.com>
2023-11-13 20:42:56 +0200

    Use TDD to fix higher page bug

--- src/api/fetchBooks.js
+++ src/api/fetchBooks.js
@@ -1,32 +1,56 @@
 const ENDPOINT = "https://www.googleapis.com/books/v1/volumes"
 const MAX_RESULT_LIMIT = 40
+const DEFAULT_RESULT_LIMIT = 10
+const DEFAULT_OFFSET = 0
 
-const fetchBooks = async ({maxResults, offset, searchTerm}) => {
-  const search = `?q=cyber ${searchTerm || ''}`
-  const normalOptions = `&maxResults=${maxResults || 10}&startIndex=${offset || 0}`
-  if (maxResults <= MAX_RESULT_LIMIT) {
-    return fetch(`${ENDPOINT}${search}${normalOptions}`)
-      .then(data => data.json())
-      .then(data => data.items)
-  } else {
-    const numberOfIterations = Math.ceil(maxResults / MAX_RESULT_LIMIT)
-    const limit = maxResults / numberOfIterations
-    let results = []
-    let iterations = []
-    for (let i = 0; i < numberOfIterations; i++) {
-      const correctedOffset = offset + limit * i
-      const correctedOptions = `&maxResults=${limit}&startIndex=${correctedOffset}`
-      iterations.push(fetch(`${ENDPOINT}${search}${correctedOptions}`)
+const fetchBooks = async ({pageSize, pageIndex, searchTerm}) => {
+  const search = `?q=cyber${' ' + searchTerm || ''}`
+  const iterationParameters = determineFetchParameters({pageSize, pageIndex})
+  let allResults = []
+  const allFetches = iterationParameters.map(iteration => {
+    const options = `&maxResults=${iteration.max || DEFAULT_RESULT_LIMIT}&startIndex=${iteration.offset || DEFAULT_OFFSET}`
+    return fetch(`${ENDPOINT}${search}${options}`)
         .then(data => data.json())
         .then(data => data.items)
         .then(books => {
-          results = [...results, ...books]
-          return books
+          allResults = [...allResults, ...books]
+          return allResults
         })
-      )
-    }
-    return Promise.all(iterations).then(_ => results)
-  }
+  })
+
+  return Promise.all(allFetches).then(_ => {
+    return allResults
+  })
 }
 
 export default fetchBooks
+
+export const determineFetchParameters = ({pageSize, pageIndex}) => {
+  const size = pageSize || DEFAULT_RESULT_LIMIT
+  const baseOffset = pageIndex ? pageIndex * size : DEFAULT_OFFSET
+  if (size < MAX_RESULT_LIMIT) {
+    return [
+      {
+        max: size,
+        offset: baseOffset
+      }
+    ]
+  }
+
+  let sizeLeft = size
+  let offsetLeft = baseOffset
+  const out = []
+  const numberOfIterations = Math.ceil(size / MAX_RESULT_LIMIT)
+  for(let i = 0; i < numberOfIterations; i++) {
+    const iterationSize = Math.min(sizeLeft, MAX_RESULT_LIMIT)
+    const pageOffset = offsetLeft
+    out.push({
+      max: iterationSize,
+      offset: pageOffset
+    })
+    sizeLeft -= MAX_RESULT_LIMIT
+    offsetLeft += iterationSize
+  }
+
+  return out
+}
